eeadfe8fd18aa248d57a798b95dc680a426849e7
Denis Stepulenok <super.denis@gmail.com>
2014-11-14 21:47:09 +0300

    Save changes

--- lesson_02/Task_Generic/src/main/java/MyList.java
+++ lesson_02/Task_Generic/src/main/java/MyList.java
@@ -3,19 +3,51 @@
  * Тип элементов - T
  */
 public class MyList<T> {
+    /**
+     * Элемент списка
+     */
+    class MyListElement {
+        T value;
+        MyListElement next;
+    }
+
+    /**
+     * Корневой элемент списка
+     */
+    MyListElement root = null;
+
     public int size() {
-        return 0;
+        int count = 0;
+        MyListElement cur = root;
+        while (cur != null) {
+            ++count;
+            cur = cur.next;
+        }
+        return count;
     }
 
     public void addToBegin(T value) {
-        // TODO: реализовать
+        MyListElement newElement = new MyListElement();
+        newElement.value = value;
+
+        // Новый элемент становится первым
+        // и ссылается на тот список, который был до операции
+        // добавления
+        newElement.next = root;
+
+        root = newElement;
     }
 
     /**
      * Вывод списка в консоль
      */
     public void show() {
-
+        for (MyListElement cur = root; cur != null; cur = cur.next) {
+            System.out.print(cur.value);
+            if (cur.next != null)
+                System.out.print(" ");
+        }
+        System.out.println();
     }
 
     /**
@@ -24,6 +56,21 @@ public class MyList<T> {
      * @param value
      */
     public void addToEnd(T value) {
-        // TODO: реализовать
+        if (root == null) {
+            addToBegin(value);
+            return;
+        }
+
+        MyListElement newElement = new MyListElement();
+        newElement.value = value;
+        // Новый элемент -> последний
+        newElement.next = null;
+
+        MyListElement cur = root;
+        while (cur.next != null)
+            cur = cur.next;
+
+        // Новый элемент добавляем в конец списка
+        cur.next = newElement;
     }
 }
