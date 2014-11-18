d19dd6d9a1d6053a2e49f9a5325ebc592152a1a7
Denis Stepulenok <super.denis@gmail.com>
2014-11-18 21:48:58 +0300

    Swing пример

--- lesson_02/SwingDemo/src/main/java/MainForm.java
+++ lesson_02/SwingDemo/src/main/java/MainForm.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
  * Основная форма
  */
 public class MainForm {
+    private static JFrame frame;
+
     private JPanel panel1;
     private JButton sumButton;
     private JTextField aField;
@@ -12,16 +14,28 @@ public class MainForm {
     private JTextField resultField;
 
     public MainForm() {
+        resultField.setVisible(false);
         sumButton.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 System.out.println("Нажали кнопку Сумма");
+                // Получаю значения из интерфейса
+                double a = Double.parseDouble(aField.getText());
+                double b = Double.parseDouble(bField.getText());
+                // Сумма чисел
+                double sum = a + b;
+                // Отправляю результат в интерфейс
+                resultField.setText(Double.toString(sum));
+
+                resultField.setVisible(true);
+
+                frame.pack();
             }
         });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("MainForm");
+        frame = new JFrame("MainForm");
         frame.setContentPane(new MainForm().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
