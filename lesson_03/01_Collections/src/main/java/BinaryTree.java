2f6f10ad2735799fb218f597a620cab8a18c14dd
Denis Tiufanov <denist2001@list.ru>
2014-11-24 20:37:06 +0300

    Добавление и поиск значений в двоичном дереве.

--- lesson_03/01_Collections/src/main/java/BinaryTree.java
+++ lesson_03/01_Collections/src/main/java/BinaryTree.java
@@ -1,17 +1,119 @@
+import javafx.scene.Parent;
+
 /**
- * Бинарное дерево
+ * Бинарное дерево поиска
+ * Левый наследник всегда меньше правого
  */
 public class BinaryTree<T> {
 
     /**
      * Узел дерева
      */
-    class TreeNode {
-        public T left;
-        public T right;
+    class TreeNode <T> {
+        public T value;
+        public TreeNode left;
+        public TreeNode right;
+    }
+
+    private TreeNode root = null;
+
+    public void add(T value) {
+
+        if (root == null) {
+            root = new TreeNode();
+            root.value = value;
+        }else {
+            TreeNode tempTreeNode = root;
+            TreeNode parentNode = null;
+            while (tempTreeNode != null) {
+                if (tempTreeNode.value.equals(value)) {
+                    System.out.println("Значение уже существует");
+                    return;
+                }else {
+                    parentNode = tempTreeNode;
+                    if (tempTreeNode.value.hashCode() < value.hashCode()) {
+                        if (tempTreeNode.right != null) {
+                            tempTreeNode = tempTreeNode.right;
+                            if (tempTreeNode.value == null) {
+                                break;
+                            } else {
+                                continue;
+                            }
+                        } else {
+                            tempTreeNode.right = new TreeNode();
+                            tempTreeNode = tempTreeNode.right;
+                            break;
+                        }
+                    }
+                    if (tempTreeNode.value.hashCode() > value.hashCode()) {
+                        if (tempTreeNode.left != null) {
+                            tempTreeNode = tempTreeNode.left;
+                            if (tempTreeNode.value == null) {
+                                break;
+                            } else {
+                                continue;
+                            }
+                        } else {
+                            tempTreeNode.left = new TreeNode();
+                            tempTreeNode = tempTreeNode.left;
+                            break;
+                        }
+
+                    }
+                }
+            }
+            TreeNode newTreeNode = new TreeNode();
+            if (parentNode == null) {
+                root = newTreeNode;
+                root.value = value;
+            }
+            else {
+                tempTreeNode = newTreeNode;
+                tempTreeNode.value = value;
+                if (parentNode.value.hashCode() > value.hashCode())
+                    parentNode.left = newTreeNode;
+                if (parentNode.value.hashCode() < value.hashCode())
+                    parentNode.right = newTreeNode;
+            }
+//            root = tempTreeNode;
+        }
+    }
+
+    public boolean find (T value) {
+        if (root == null) {
+            System.out.println("Дерево пустое");
+            return false;
+        }else {
+            TreeNode tempTreeNode = root;
+            while (tempTreeNode != null) {
+                if (tempTreeNode.value.equals(value)) {
+                    System.out.println("Значение найдено");
+                    return true;
+                }else {
+                    if (tempTreeNode.value.hashCode() < value.hashCode()) {
+                        if (tempTreeNode.right != null) {
+                            tempTreeNode = tempTreeNode.right;
+                            continue;
+                        } else {
+                            System.out.println("Значениея не существует");
+                            return false;
+                        }
+                    }
+                    if (tempTreeNode.value.hashCode() > value.hashCode()) {
+                        if (tempTreeNode.left != null) {
+                            tempTreeNode = tempTreeNode.left;
+                            continue;
+                        } else {
+                            System.out.println("Значениея не существует");
+                            return false;
+                        }
+
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     // TODO: реализовать
-
-
 }
--- lesson_03/01_Collections/src/test/java/BinaryTreeTest.java
+++ lesson_03/01_Collections/src/test/java/BinaryTreeTest.java
@@ -1,16 +1,61 @@
 import org.junit.Test;
 
+import javax.swing.tree.TreeNode;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
 /**
- * Список элементов
+ * Тестирование дерева поиска
  */
 public class BinaryTreeTest {
     /**
-     * Список
+     * Тестирование добавления в дерево поиска элементов
      */
     @Test
-    public void lists() {
+    public void testTree() {
         // Интерфейс =  Класс с реализацией этого интерфейса
+
         BinaryTree<Integer> ints = new BinaryTree<Integer>();
+        ints.add(3);
+        ints.add(8);
+        ints.add(1);
+        ints.add(5);
+        ints.add(1);
+        assertTrue(ints.find(5));
     }
+
+    /**
+     * Большой случайный тест
+     *//*
+    @Test
+    public void testBig() {
+        // Генератор случайных чисел
+        Random random = new Random();
+        // Проверочный set
+        Set<Integer> test = new HashSet<Integer>();
+        SearchTree<Integer> tree = new BinaryTree<Integer>();
+        assertEquals("Пока что дерево пустое", 0, tree.deep());
+        int valueHit = 0;
+        int valueNotFound = 0;
+        for (int i = 0; i < 1000; i++) {
+            int value = random.nextInt(10000);
+            test.add(value);
+            tree.add(value);
+            assertTrue(tree.find(value));
+
+            int value2 = random.nextInt(10000);
+            if (test.contains(value2)) {
+                assertTrue(tree.find(value2));
+                valueHit++;
+            } else {
+                assertFalse(tree.find(value2));
+                valueNotFound++;
+            }
+        }
+        System.out.println("value: hit " + valueHit + ", not found: " + valueNotFound);
+    }*/
 
 }
