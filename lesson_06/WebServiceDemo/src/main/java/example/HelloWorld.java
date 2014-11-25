fe7cf6b8494fcbe308ac8c296b91f83788b80892
Denis Stepulenok <super.denis@gmail.com>
2014-11-25 21:31:31 +0300

    Урок 6

--- lesson_06/WebServiceDemo/src/main/java/example/HelloWorld.java
+++ lesson_06/WebServiceDemo/src/main/java/example/HelloWorld.java
@@ -9,16 +9,16 @@ import javax.xml.ws.Endpoint;
  */
 @WebService()
 public class HelloWorld {
+    public static void main(String[] argv) {
+        Object implementor = new HelloWorld();
+        String address = "http://localhost:9000/HelloWorld";
+        Endpoint.publish(address, implementor);
+    }
+
     @WebMethod
     public String sayHelloWorldFrom(String from) {
         String result = "Hello, world, from " + from;
         System.out.println(result);
         return result;
-    }
-
-    public static void main(String[] argv) {
-        Object implementor = new HelloWorld();
-        String address = "http://localhost:9000/HelloWorld";
-        Endpoint.publish(address, implementor);
     }
 }
